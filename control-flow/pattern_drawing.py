2111b4ecfe1f2a40763cf24094aead0e44ebeedf
Amodu Oreoluwa <amoduoreoluwa20@gmail.com>
2025-09-24 15:50:51 +0100

    add new changes

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,6 +2,6 @@ size = int(input("Enter the size of the pattern:"))
 row = 0
  while row>size:
     for col in range(size):
-        print("*", end"")
+        print("*", end ="")
     print()
     row+=1
