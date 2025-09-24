52643275b7e4041ccb5595c18309fba8e8bfb9da
Amodu Oreoluwa <amoduoreoluwa20@gmail.com>
2025-09-24 03:06:26 +0100

    made some changes

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -5,5 +5,5 @@
     print("Don't forget your umbrella and a raincoat.")
  elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
- else :
+ else:
     print(" Sorry, I don't have recommendations for this weather.")
