4e052849208fa71628871f40215a4a8bcd5e7c5c
Amodu Oreoluwa <amoduoreoluwa20@gmail.com>
2025-09-27 05:42:19 +0100

    added another file

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,39 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            item= input("Enter the name of an item you want")
+            shopping_list.append(item)
+            print(f"{item} has been added into your list")
+        elif choice == '2':
+            item = input("enter the item to remove")
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print(f"item has been removed")
+            else:
+                print("item cannot be found")
+        elif choice == '3':
+            if shopping_list:
+                print("your shopping list")
+                for i in shopping_list:
+                    print("-",item)
+            else:
+                print("your shopping list is empty")
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
