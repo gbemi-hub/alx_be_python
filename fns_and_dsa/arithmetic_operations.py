1dd1053be322d046e4b7c0f827f2999a0fc15206
Amodu Oreoluwa <amoduoreoluwa20@gmail.com>
2025-09-27 05:02:04 +0100

    a new folder

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,14 @@
+def perform_operation(num1,num2,operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation = "subtract":
+        return num1-num2:
+    elif operation == "multipy":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "error,division by zero is not allowed."
+        else:
+            return num1 / num2
+    else:
+        return "make sure you perform a specific operation"
