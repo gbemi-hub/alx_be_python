523f557e78673064baf123f2de7bb389b72fbae6
Amodu Oreoluwa <amoduoreoluwa20@gmail.com>
2025-09-27 05:10:25 +0100

    made some changes

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,8 +1,8 @@
 def perform_operation(num1,num2,operation):
     if operation == "add":
         return num1 + num2
-    elif operation = "subtract":
-        return num1-num2:
+    elif operation == "subtract":
+        return num1-num2
     elif operation == "multipy":
         return num1 * num2
     elif operation == "divide":
