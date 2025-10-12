b3b7b49d01d1ef5dbe999a114baa7fe43b2b36d9
Amodu Oreoluwa <amoduoreoluwa20@gmail.com>
2025-10-12 20:07:39 +0100

    new directory

---
+++ oop/book_class.py
@@ -0,0 +1,15 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"'{self.title}' by {self.author}"
+
+    def __repr__(self):
+        return f"Book(title='{self.title}', author='{self.author}')"
+
+book = Book("1984", "George Orwell")
+
+print(book)          
+print(repr(book))
