8d60972b53adf2d5231f32da35091e09ab2fb4f1
Amodu Oreoluwa <amoduoreoluwa20@gmail.com>
2025-10-12 20:48:06 +0100

    made some vhanges

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,15 +1,17 @@
 class Book:
-    def __init__(self, title, author):
+    def __init__(self, title, author,year):
         self.title = title
         self.author = author
+         self.year = year
+        
 
     def __str__(self):
-        return f"'{self.title}' by {self.author}"
+        return f"'{self.title}' by {self.author} in {self.year} "
 
     def __repr__(self):
-        return f"Book(title='{self.title}', author='{self.author}')"
+        return f"Book(title='{self.title}', author='{self.author}, year={self.year}')"
 
-book = Book("1984", "George Orwell")
+book = Book("1984", "George Orwell" , "2004")
 
 print(book)          
 print(repr(book))
